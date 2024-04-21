9d98a0300f77ffb50ee0438066712e011351924b
heehyeok <ghdgmlgur0514@gmail.com>
2024-04-21 22:00:21 +0900

    성적관리프로그램 클래스를 이용한 버전 추가

--- avg_to_grade_with_dict.py
+++ avg_to_grade_with_dict.py
@@ -3,190 +3,185 @@
 #            ,{"name":"e","student_id":"2020","english_score":40,"c_score":80,"python_score": 60}
 #            ,{"name":"f","student_id":"2020","english_score":30,"c_score":80,"python_score": 60}
 #            ,{"name":"g","student_id":"2021","english_score":100,"c_score":80,"python_score": 60}]
-rank = []
-student = []
 
-def get_info(student):
-    name = input("학생의 이름 :")
-    id = input("학생의 학번 :")
-    english_score = int(input("학생의 영어 점수 :"))
-    c_score = int(input("학생의 C언어 점수 :"))
-    python_score = int(input("학생의 Python 점수 :"))
+class Student:
+    def __init__(self,name,id,english_score,c_score,python_score):
+        self.name = name
+        self.id = id
+        self.english_score = english_score
+        self.c_score = c_score
+        self.python_score = python_score
+        self.total = english_score + c_score + python_score
+        self.avg = (english_score + c_score + python_score) / 3
 
-    student_dict = dict()
-    student_dict["name"] = name
-    student_dict["student_id"] = id
-    student_dict["english_score"] = english_score
-    student_dict["c_score"] = c_score
-    student_dict["python_score"] = python_score
+    def get_grade(self):
+            if (self.avg >= 90):
+                self.grade = 'A'
+
+            elif (self.avg >= 80):
+                self.grade = 'B'
+
+            elif (self.avg >= 70):
+                self.grade = 'C'
+
+            elif (self.avg >= 60):
+                self.grade = 'D'
+
+            else:
+                self.grade = 'F'
+
+    def get_rank(self,rank):
+        self.rank = rank
+
+
+        
+class IO_program():
+    def __init__(self):
+        self.student_list = []
+        self.rank = []
+
+    def get_info(self):
+        name = input("학생의 이름 :")
+        id = input("학생의 학번 :")
+        english_score = int(input("학생의 영어 점수 :"))
+        c_score = int(input("학생의 C언어 점수 :"))
+        python_score = int(input("학생의 Python 점수 :"))
+
+        student = Student(name,id,english_score,c_score,python_score)
+        self.student_list.append(student)
+
+
+    def sorted_rank(self): #평균 점수를 통해서 
+        total_score = []
+        for student in (self.student_list):
+            total_score.append(student.total)
+
+        self.rank = [sorted(total_score, reverse = True).index(i) for i in total_score]
+        
+        i = 0
+        for student in (self.student_list):
+            student.get_rank(self.rank[i])
+            i += 1
+
+        return self.rank
     
-    student.append(student_dict)
+    def print_rank(self):
+        i = 0
+        for student in self.student_list:
+            print(f"{student.name}학생의 등수는 {self.rank[i]+1}")
+            i += 1
+    
+    def remove_student(self,student_id):
+        i = 0
+        found = 0
+        for student in (self.student_list):
+            if (student.id == student_id):
+                found = 1
+                del self.student_list[i]
+                print(f"{student_id}학번 학생을 리스트에서 제거했습니다.")
+        if (found ==0 ):
+            print(f"{student_id}학번 학생은 리스트에 존재하지 않습니다.")
 
-    return student
-
-def calculate(student):
-    for i in range(len(student)):
-        total = student[i]["english_score"] + student[i]["c_score"] + student[i]["python_score"] 
-        student[i]["total"] = total
-
-    for i in range(len(student)):
-        student[i]['avg'] = student[i]["total"] / 3
-
-    return student
-
-def get_grade(student):
-    for i in range(len(student)):
-        if (student[i]['avg'] >= 90):
-            student[i]['grade'] = 'A'
-
-        elif (student[i]['avg'] >= 80):
-            student[i]['grade'] = 'B'
-
-        elif (student[i]['avg'] >= 70):
-            student[i]['grade'] = 'C'
-
-        elif (student[i]['avg'] >= 60):
-            student[i]['grade'] = 'D'
+        
+    def search_student(self,name,id):
+        found = 0
+        for student in (self.student_list):
+            if (student.name == name) and (student.id ==id):
+                found = 1
+        if(found == 1):
+            print(f"이름이 {name}, 학번이 {id}인 학생은 존재합니다.")
 
         else:
-            student[i]['grade'] = 'F'
-    return student
+            print(f"이름이 {name}, 학번이 {id}인 학생은 존재하지 않습니다.")
 
-def sorted_rank(student): #평균 점수를 통해서 
-    total_score = []
-    for i in range(len(student)):
-        total_score.append(student[i]["total"])
+    def count_over_80(self):
+        count = 0
+        subject = input("과목을 입력해주세요.")
+        if subject == "python":
+            for student in (self.student_list):
+                if student.python_score >= 80:
+                    count += 1
+        elif subject == "c":
+            for student in (self.student_list):
+                if student.c_score >= 80:
+                    count += 1
+        elif subject == "english":
+            for student in (self.student_list):
+                if student.english_score >= 80:
+                    count += 1
+        else:
+            print(f"{subject}은 존재하지 않습니다.")
+        print(f"{subject}에서 80점 이상 학생수는 {count}명입니다.")
 
-    rank = [sorted(total_score, reverse = True).index(i) for i in total_score]
-    return rank
+    def insert_student(self):
+        self.get_info()
+
+        index = int(input("몇 번째에 학생 정보를 입력할지 입력해주세요: "))
+        student = self.student_list.pop()
+        self.student_list.insert(index,student)
         
 
-def insert_student(student):
-    student_dict = dict()
-    name = input("학생의 이름 :")
-    id = input("학생의 학번 :")
-    english_score = int(input("학생의 영어 점수 :"))
-    c_score = int(input("학생의 C언어 점수 :"))
-    python_score = int(input("학생의 python 점수 :"))
-
-    student_dict['name'] = name
-    student_dict["student_id"] = id
-    student_dict["english_score"]= english_score
-    student_dict["c_score"]= c_score
-    student_dict["python_score"] = python_score
-
-    calculate(student)
-    get_grade(student)
-
-    index = int(input("몇 번째에 학생 정보를 입력할지 입력해주세요: "))
-
-    student.insert(index,student_dict)
-    return student
-
-def remove_student(student,student_id):
-    for i in range(len(student)):
-        if student[i]["student_id"] == student_id:
-            del student[i]
-            print(f"{student_id}학번 학생의 정보를 삭제했습니다.")
-        
-    return student
-
-def search_student(student,name,id):
-    found = 0
-    for i in range(len(student)):
-        if (student[i]['name'] == name) and (student[i]["student_id"]==id):
-            found = 1
-    if(found == 1):
-        print(f"이름이 {name}, 학번이 {id}인 학생은 존재합니다.")
-
-    else:
-        print(f"이름이 {name}, 학번이 {id}인 학생은 존재하지 않습니다.")
-
-def count_over_80(student,subject):
-    count = 0
-    for i in range(len(student)):
-        if student[i][subject] >= 80:
-            count += 1
-
-    print(f"{subject}에서 80점 이상 학생수는 {count}명입니다.")
-
-def sort_total(student):
-    print("총점을 기준으로 학생들을 정렬했습니다.")
-    return sorted(student ,key= lambda student: (student['total']))
-
-def print_info(student):
-    print("성적 관리 프로그램")
-    print("=" * 94)
-    print("학번\t이름\t영어\tc-언어\t파이썬\t총점\t평균\t학점\t등수")
-    print("=" * 94)
-    for i in range(len(student)):
-        print(f"{student[i]["student_id"]}\t{student[i]["name"]}\t{student[i]["english_score"]}\t{student[i]["c_score"]}\t{student[i]["python_score"]}\t{student[i]["total"]}\t{student[i]["avg"]: 4.2f}\t{student[i]["grade"]}\t{rank[i]+1}\t")
+    def print_info(self):
+        print("성적 관리 프로그램")
+        print("=" * 94)
+        print("학번\t이름\t영어\tc-언어\t파이썬\t평균\t총점\t학점\t등수")
+        print("=" * 94)
+        i = 0
+        self.sorted_rank()
+        for student in (self.student_list):
+            student.get_grade()
+            print(f"{student.id}\t{student.name}\t{student.english_score}\t{student.c_score}\t{student.python_score}\t{student.total}\t{student.grade}\t{student.rank + 1}")
 
 
 #main
 
 program_continue = 1
+program = IO_program()
+
 while(program_continue != 0):
+    print("=" * 94)
     print("<성적 변환기 프로그램>")
     print("1.새로운 학생 입력")
-    print("2.총점/평균 계산")
-    print("3.학점 계산")
-    print("4.등수 계산")
-    print("5.학생 정보 출력")
-    print("6.새로운 학생 삽입")
-    print("7.학생 정보 삭제")
-    print("8.탐색함수(학번,이름 입력)")
-    print("9.총점을 기준으로 정렬")
-    print("10. 80점 이상 학생 수 카운트")
+    print("2.등수 계산")
+    print("3.학생 정보 출력")
+    print("4.새로운 학생 삽입")
+    print("5.학생 정보 삭제")
+    print("6.탐색함수(학번,이름 입력)")
+    print("7. 80점 이상 학생 수 카운트")
     print("0.프로그램 종료")
+    print("=" * 94)
 
     program_continue = int(input("어떤 기능을 사용할 지 입력해주세요. "))
 
     if (program_continue == 1):
-        student = get_info(student)
-        continue
-
-    elif (program_continue == 2):
-        student =calculate(student)
-        continue
+        program.get_info()
+             
+    elif(program_continue == 2):
+        program.sorted_rank()
+        program.print_rank()
+        
 
     elif (program_continue == 3):
-        student =get_grade(student)
-        continue
-
-    elif(program_continue == 4):
-        rank = sorted_rank
-        continue
-
-    elif (program_continue == 5):
-        rank = sorted_rank
-        print_info(student)
-        continue
-    
-    elif (program_continue == 6):
-        insert_student(student)
-        continue
-
-    elif(program_continue == 7):
+        program.print_info()
+        
+    elif (program_continue == 4):
+        program.insert_student()
+        
+    elif(program_continue == 5):
         student_id = input("삭제하고 싶은 학생의 학번을 입력해주세요 :")
-        remove_student(student,student_id)
-        continue
-
-    elif(program_continue == 8):
+        program.remove_student(student_id)
+        
+    elif(program_continue == 6):
         student_id = input("찾고 싶은 학생의 학번을 입력해주세요 :")
         name = input("찾고 싶은 학생의 이름을 입력해주세요 :")
-        search_student(student,name,student_id)
-        continue
+        program.search_student(name,student_id)
 
-    elif(program_continue == 9):
-        sort_total(student)
-        continue
-
-    elif(program_continue == 10):
-        subject = input("확인하고 싶은 과목명을 입력해주세요 :")
-        count_over_80(student,subject)
-        continue
-
+    elif(program_continue == 7):
+        program.count_over_80()
+        
     else:
         program_continue == 0
         print("프로그램을 종료하겠습니다.")
+
+    print("=" * 94)
+    print("\n\n\n\n")
